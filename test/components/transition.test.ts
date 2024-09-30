c0da348385c59e82c4973ff93eb6f34070a3f0b9
Dinh-Van Colomban <van.colomban@gmail.com>
2024-09-30 22:53:41 +0200

    test(wip): adds todos for component testing

---
+++ test/components/transition.test.ts
@@ -0,0 +1,6 @@
+import { describe, it } from 'vitest';
+
+describe('routeTransition', () => {
+  it.todo('should render a transition container when a transition object is provided');
+  it.todo('should not render a transition container when a transition object is not provided');
+});
