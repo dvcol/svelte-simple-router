a3ab893c508f875c1ff0bdf9c00a2ead582f9ab9
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2024-12-06 15:14:14 -0600

    export link types

--- src/lib/models/index.ts
+++ src/lib/models/index.ts
@@ -2,6 +2,7 @@ export * from './error.model.js';
 export * from './matcher.model.js';
 export * from './route.model.js';
 export * from './router.model.js';
+export * from './link.model.js';
 export * from './view.model.js';
 export * from './navigation.model.js';
 export * from './component.model.js';
