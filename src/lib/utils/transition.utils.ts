23f608fc426dd6b2a7cb5726a3ea95911d4313d8
Dinh-Van Colomban <van.colomban@gmail.com>
2025-01-17 10:17:12 +0100

    chore(deps): bump dependencies

--- src/lib/utils/transition.utils.ts
+++ src/lib/utils/transition.utils.ts
@@ -1,10 +1,10 @@
-import { scaleFadeInOut, type ScaleFadeParams } from '@dvcol/svelte-utils/transition';
+import { scaleFreeze, type ScaleFreezeParams } from '@dvcol/svelte-utils/transition';
 
 import type { TransitionProps } from '~/models/component.model.js';
 
-export const transition: TransitionProps<{ in?: ScaleFadeParams; out?: ScaleFadeParams }> = {
-  in: scaleFadeInOut,
-  out: scaleFadeInOut,
+export const transition: TransitionProps<{ in?: ScaleFreezeParams; out?: ScaleFreezeParams }> = {
+  in: scaleFreeze,
+  out: scaleFreeze,
   params: { in: { delay: 400 } },
   skipFirst: true,
 };
